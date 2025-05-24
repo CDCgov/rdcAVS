ab186fcd1b38cfa64cfa9a5a7a9145c37473362d
Mervin Keith Cuadera <mkcuadera@gmail.com>
2025-05-23 22:09:52 -0400

    remove inst
    looks like upon installation folders in inst go up a directory...interesting behavior

--- R/app.R
+++ R/app.R
@@ -559,7 +559,7 @@ campagneApp <- function(...) {
             antenne_target = input$selected_ant,
             zs_target = input$selected_zs,
             gdb = geo_data_reactive(),
-            zs_masque = system.file("inst", "extdata", "zs_masque_template.xlsx",
+            zs_masque = system.file("extdata", "zs_masque_template.xlsx",
                                     package = "rdcAVS"),
             output_folder = selected_dir()
           )
